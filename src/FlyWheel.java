db09c4ef104f5d7c81a47e1eb38271e75f81909b
copo <copo876@gmail.com>
2016-07-26 22:30:48 -0400

    First commit, some basic files

---
+++ src/FlyWheel.java
@@ -0,0 +1,22 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants
+
+public class FlyWheel {
+
+  JFrame footballField;
+  MarchingDude smellyFreshman;
+
+  public static void main(String args[]) {
+
+    // TODO Set up the football field
+    footballField = new JFrame("footballField");
+
+    // TODO Set up the smelly freshman
+    smellyFreshman = new MarchingDude();
+
+    // TODO tap off... begin the show...
+    
+
+  }
+
+}
---
+++ src/MarchingDude.java
@@ -0,0 +1,17 @@
+import java.time;
+
+public class MarchingDude {
+
+  private int PPI; //This is basically his health level, it decreases every time the director glares at him
+  private int wynenPoints;
+
+  public MarchingDude() {
+    this.PPI = 10 0;
+    this.wynenPoints = 0;
+  }
+
+  public long step(Clock dermlern) {
+    return dermlern.millis();
+  }
+
+}
